9cccb45728dad2cbf627d3cecf183f83f76f5ead
Andrii <andrii_bui@epam.com>
2022-03-14 16:13:56 +0100

    added behaviors

---
+++ CrossCuttingSample/Api/Infrastructure/CrossCuttingConcerns/DefaultBehavior.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Api.Infrastructure.CrossCuttingConcerns
+{
+    public class DefaultBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
