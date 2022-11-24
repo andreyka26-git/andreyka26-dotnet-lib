c4cbdc8cb48a03c34e20a8c9cff5f94ccf5463e8
Andrii Bui <andriibui@microsoft.com>
2022-11-24 18:14:04 +0100

    initial github custom auth

---
+++ AuthorizationSample/Custom/GithubCustom/AuthorizeService.cs
@@ -0,0 +1,31 @@
+﻿namespace GithubCustom
+{
+    public class AuthorizeService
+    {
+        // we need to validate this state is the same per client's oauth flow
+        private const string State = "qwerty";
+
+        private IConfiguration _config;
+
+        public AuthorizeService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<string> GetAuthTokenAsync(CallbackResponse callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateAuthorizeLink()
+        {
+            var clientId = _config.GetValue<string>("ClientId");
+            var clientSecret = _config.GetValue<string>("ClientSecret");
+            var redirectUrl = _config.GetValue<string>("RedirectUrl");
+
+            var link = $"https://github.com/login/oauth/authorize?client_id={clientId}&redirect_uri={redirectUrl}&scope=user&state={State}";
+
+            return link;
+        }
+    }
+}
---
+++ AuthorizationSample/Custom/GithubCustom/Controllers/AuthorizeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GithubCustom.Controllers
+{
+    [ApiController]
+    public class AuthorizeController : ControllerBase
+    {
+        private readonly AuthorizeService _authorizeService;
+
+        public AuthorizeController(AuthorizeService authorizeService)
+        {
+            _authorizeService = authorizeService;
+        }
+
+        [HttpGet("/signin-github")]
+        public async Task<IActionResult> HandleGetCallbackAsync([FromQuery] string code, [FromQuery] string state)
+        {
+            var callback = new CallbackResponse
+            {
+                AuthCode = code,
+                State = state
+            };
+
+            var token = await _authorizeService.GetAuthTokenAsync(callback);
+
+            //do whatever needed with token
+
+            // this one is triggered
+            return Ok();
+        }
+
+        [HttpPost("/signin-github")]
+        public async Task<IActionResult> HandlePostCallbackAsync([FromQuery] string code, [FromQuery] string state)
+        {
+            var callback = new CallbackResponse
+            {
+                AuthCode = code,
+                State = state
+            };
+
+            var token = await _authorizeService.GetAuthTokenAsync(callback);
+
+            //do whatever needed with token
+            return Ok();
+        }
+    }
+}
