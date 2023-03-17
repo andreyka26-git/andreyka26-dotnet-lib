cf71f17af49efd6cce80922c11822edcae696942
Andrii Bui <andriibui@microsoft.com>
2023-03-17 14:02:37 +0100

    added oidc auth server solution

---
+++ AuthorizationSample/OAuthAndOpenIdConnect/Oidc.OpenIddict.AuthorizationServer/Controllers/AuthorizationController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Immutable;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+using System.Security.Claims;
+using System.Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Oidc.OpenIddict.AuthorizationServer;
+
+namespace Oidc.OpenIddict.AuthorizationServer.Controllers
+{
+    [ApiController]
+    public class AuthorizationController : Controller
+    {
+        private readonly IOpenIddictApplicationManager _applicationManager;
+        private readonly IOpenIddictAuthorizationManager _authorizationManager;
+        private readonly IOpenIddictScopeManager _scopeManager;
+        private readonly AuthorizationService _authService;
+
+        public AuthorizationController(
+            IOpenIddictApplicationManager applicationManager,
+            IOpenIddictAuthorizationManager authorizationManager,
+            IOpenIddictScopeManager scopeManager,
+            AuthorizationService authService)
+        {
+            _applicationManager = applicationManager;
+            _authorizationManager = authorizationManager;
+            _scopeManager = scopeManager;
+            _authService = authService;
+        }
+
+        [HttpGet("~/connect/authorize")]
+        [HttpPost("~/connect/authorize")]
+        public async Task<IActionResult> Authorize()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+            var parameters = _authService.ParseOAuthParameters(HttpContext, new List<string> { Parameters.Prompt });
+
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!_authService.IsAuthenticated(result, request))
+            {
+                return Challenge(properties: new AuthenticationProperties
+                {
+                    RedirectUri = _authService.BuildRedirectUrl(HttpContext.Request, parameters)
+                }, new[] { CookieAuthenticationDefaults.AuthenticationScheme });
+            }
+
+            var application = await _applicationManager.FindByClientIdAsync(request.ClientId) ??
+                              throw new InvalidOperationException("Details concerning the calling client application cannot be found.");
+
+            var consentClaim = result.Principal.GetClaim(Consts.ConsentNaming);
+
+            if (consentClaim != Consts.GrantAccessValue)
+            {
+                var returnUrl = HttpUtility.UrlEncode(_authService.BuildRedirectUrl(HttpContext.Request, parameters));
+                var consentRedirectUrl = $"/Consent?returnUrl={returnUrl}";
+
+                return Redirect(consentRedirectUrl);
+            }
+
+            var userId = result.Principal.FindFirst(ClaimTypes.Email)!.Value;
+
+            var identity = new ClaimsIdentity(
+                authenticationType: TokenValidationParameters.DefaultAuthenticationType,
+                nameType: Claims.Name,
+                roleType: Claims.Role);
+
+            identity.SetClaim(Claims.Subject, userId)
+                .SetClaim(Claims.Email, userId)
+                .SetClaim(Claims.Name, userId)
+                .SetClaims(Claims.Role, new List<string> { "user", "admin" }.ToImmutableArray());
+
+            identity.SetScopes(request.GetScopes());
+            identity.SetResources(await _scopeManager.ListResourcesAsync(identity.GetScopes()).ToListAsync());
+
+            var authorizations = await _authorizationManager
+                .FindAsync(
+                    subject: userId,
+                    client: await _applicationManager.GetIdAsync(application),
+                    status: Statuses.Valid,
+                    type: AuthorizationTypes.Permanent,
+                    scopes: request.GetScopes())
+                .ToListAsync();
+
+            var authorization = authorizations.LastOrDefault();
+
+            authorization ??= await _authorizationManager.CreateAsync(
+                identity: identity,
+                subject: userId,
+                client: await _applicationManager.GetIdAsync(application),
+                type: AuthorizationTypes.Permanent,
+                scopes: identity.GetScopes());
+
+            identity.SetAuthorizationId(await _authorizationManager.GetIdAsync(authorization));
+            identity.SetDestinations(AuthorizationService.GetDestinations);
+
+            return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+
+        [HttpPost("~/connect/token")]
+        public async Task<IActionResult> Exchange()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+            if (!request.IsAuthorizationCodeGrantType() && !request.IsRefreshTokenGrantType())
+                throw new InvalidOperationException("The specified grant type is not supported.");
+
+            var result =
+                await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+            var userId = result.Principal.GetClaim(Claims.Subject);
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Forbid(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.InvalidGrant,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
+                            "Cannot find user from the token."
+                    }));
+            }
+
+            var identity = new ClaimsIdentity(result.Principal.Claims,
+                authenticationType: TokenValidationParameters.DefaultAuthenticationType,
+                nameType: Claims.Name,
+                roleType: Claims.Role);
+
+            identity.SetClaim(Claims.Subject, userId)
+                .SetClaim(Claims.Email, userId)
+                .SetClaim(Claims.Name, userId)
+                .SetClaims(Claims.Role, new List<string> { "user", "admin" }.ToImmutableArray());
+
+            identity.SetDestinations(AuthorizationService.GetDestinations);
+
+            return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+
+        [HttpPost("~/connect/logout")]
+        public async Task<IActionResult> LogoutPost()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return SignOut(
+                authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                properties: new AuthenticationProperties
+                {
+                    RedirectUri = "/"
+                });
+        }
+    }
+}
---
+++ AuthorizationSample/OAuthAndOpenIdConnect/Oidc.OpenIddict.AuthorizationServer/Pages/Consent.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+
+namespace Oidc.OpenIddict.AuthorizationServer.Pages;
+
+[Authorize]
+public class Consent : PageModel
+{
+    [BindProperty]
+    public string? ReturnUrl { get; set; }
+
+    public IActionResult OnGet(string returnUrl)
+    {
+        ReturnUrl = returnUrl;
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(string grant)
+    {
+        if (grant == Consts.GrantAccessValue)
+        {
+            var consentClaim = User.GetClaim(Consts.ConsentNaming);
+
+            if (string.IsNullOrEmpty(consentClaim))
+            {
+                User.SetClaim(Consts.ConsentNaming, Consts.GrantAccessValue);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, User);
+            }
+
+            return Redirect(ReturnUrl);
+        }
+
+        return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+    }
+}
