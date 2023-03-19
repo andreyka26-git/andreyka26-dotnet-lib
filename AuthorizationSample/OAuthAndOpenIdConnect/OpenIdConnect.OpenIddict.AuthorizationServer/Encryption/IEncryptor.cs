f8fc3c1e923f5675f0ede21395b706b02f3b0e6e
andreyka26-git <b.andriy.b2000@gmail.com>
2023-03-19 11:24:08 +0100

    removed old oidc impl

--- AuthorizationSample/OAuthAndOpenIdConnect/OpenIdConnect.OpenIddict.AuthorizationServer/Encryption/IEncryptor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenIdConnect.OpenIddict.AuthorizationServer.Encryption
-{
-    public interface IEncryptor
-    {
-        (string name, byte[] content) GenerateEncryptionCertificate();
-        (string name, byte[] content) GenerateSigningCertificate();
-    }
-}
